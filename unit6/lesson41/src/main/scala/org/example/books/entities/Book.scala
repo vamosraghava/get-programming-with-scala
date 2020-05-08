6e86a8c432238c390e971d3ae2d27a6faefdd5f5
Daniela Sfregola <daniela.sfregola@gmail.com>
2020-05-08 12:25:07 +0100

    [unit 6] More code for lesson 41

--- unit6/lesson41/src/main/scala/org/example/books/entities/Book.scala
+++ unit6/lesson41/src/main/scala/org/example/books/entities/Book.scala
@@ -3,9 +3,13 @@ package org.example.books.entities
 import scala.util.{Failure, Success, Try}
 import java.net.URL
 
-case class Book(id: Long, title: String, authors: List[String], imageUrl: Option[URL]) {
+case class Book(id: Long,
+                title: String,
+                authors: List[String],
+                imageUrl: Option[URL]) {
 
-  def toPrettyString: String = s"[$id] $title ${authors.mkString("{", ", ", "}")}"
+  def toPrettyString: String =
+    s"[$id] $title ${authors.mkString("{", ", ", "}")}"
 }
 
 object Book {
@@ -22,27 +26,35 @@ object Book {
       Book(id, title, authors, imageUrl)
     }
 
-  private def parseLong(row: Map[String, String], key: String): Try[Long] =
+  private def parseLong(row: Map[String, String],
+                        key: String): Try[Long] =
     parseAs(row, key, _.toLong)
 
-  private def parseString(row: Map[String, String], key: String): Try[String] =
+  private def parseString(row: Map[String, String],
+                          key: String): Try[String] =
     parseAs(row, key, x => x)
 
-  private def parseStrings(row: Map[String, String], key: String): Try[List[String]] =
+  private def parseStrings(row: Map[String, String],
+                           key: String): Try[List[String]] =
     parseAs(row, key, _.split(",").map(_.trim).toList)
 
-  private def parseURL(row: Map[String, String], key: String): Try[URL] =
+  private def parseURL(row: Map[String, String],
+                       key: String): Try[URL] =
     parseAs(row, key, s => new URL(s))
 
-  private def parseAs[T](row: Map[String, String], key: String, parser: String => T): Try[T] =
+  private def parseAs[T](row: Map[String, String],
+                         key: String, parser: String => T): Try[T] =
     for {
-      value <- getValueOrFail(row, key)
+      value <- getValue(row, key)
       t <- Try(parser(value))
     } yield t
 
-  private def getValueOrFail(row: Map[String, String], key: String): Try[String] =
+  private def getValue(row: Map[String, String],
+                       key: String): Try[String] =
     row.get(key) match {
       case Some(value) => Success(value)
-      case None => Failure(new IllegalArgumentException(s"Couldn't find column $key in row - row was $row"))
+      case None => Failure(new IllegalArgumentException(
+        s"Couldn't find column $key in row - row was $row"))
     }
+
 }
